a05442286029fc4cfc60e519b251d49cbed317f1
abirHazari <abirhazaria@gmail.com>
2023-08-18 02:14:31 +0600

    finished etch-a-sketch base

---
+++ main.js
@@ -0,0 +1,41 @@
+let numOfGrid = 32;
+const container = document.querySelector('#container');
+const resetButton = document.querySelector('.reset-button button');
+
+const createGrid = (numOfGrid) => {
+  const tempDiv = document.createElement('div');
+  tempDiv.classList.add('temp-div');
+
+  for (let i = 0; i < numOfGrid; i++) {
+    const rowDivs = document.createElement('div');
+    rowDivs.classList.add('grid-row');
+
+    for (let j = 0; j < numOfGrid; j++) {
+      const witdhAndHeight = 960 / numOfGrid;
+      const boxDivs = document.createElement('div');
+      boxDivs.classList.add('grid-box');
+      boxDivs.style.width = `${witdhAndHeight}px`;
+      boxDivs.style.height = `${witdhAndHeight}px`;
+      boxDivs.addEventListener('mouseenter', () => {
+        boxDivs.style.backgroundColor = 'black';
+      })
+      rowDivs.appendChild(boxDivs);
+    }
+    tempDiv.appendChild(rowDivs);
+  }
+  container.appendChild(tempDiv);
+}
+
+resetButton.addEventListener('click', () => {
+  let inputNumber = +prompt("Enter number of grids (max 100)","");
+  while (inputNumber >= 100) {
+    inputNumber = +prompt("Enter a number smaller or equal to 100", "");
+  }
+  const tempDiv = document.querySelector('.temp-div');
+
+  tempDiv.remove();
+
+  createGrid(inputNumber);
+})
+
+createGrid(numOfGrid);
